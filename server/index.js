cd29aa780588d49d1534e35679396f02fb000560
Patryk Oltuch <95oltuchpatryk@gmail.com>
2022-08-26 18:03:12 +0200

    created description

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 
+
+/* database section - START */
 const mongoose = require('mongoose');
 const dbKeys = require('../db/dbKeys');
-
 const { login, password } = dbKeys;
 mongoose.connect(`mongodb+srv://${login}:${password}@cluster0.stmjhgd.mongodb.net/?retryWrites=true&w=majority`);
 
 const db = mongoose.connection;
 db.on('err', console.error.bind(console, 'connection error:'));
+/* database section - STOP */
 
 const PORT = process.env.PORT || 3001;
 
