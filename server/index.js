9ba43d11a15a320646b6fb1ef0dc5ac5ee581885
Patryk Oltuch <95oltuchpatryk@gmail.com>
2022-08-29 16:54:58 +0200

    commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const express = require("express");
 
 /* database section - START */
 const mongoose = require('mongoose');
+const singleResult = require('../db/singleResult.model');
 const dbKeys = require('../db/dbKeys');
+
+
 const { login, password } = dbKeys;
 mongoose.connect(`mongodb+srv://${login}:${password}@cluster0.stmjhgd.mongodb.net/?retryWrites=true&w=majority`);
 
@@ -18,6 +21,15 @@ const app = express();
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
+
+app.get('/db', (req,res) => {
+
+    singleResult.find({}, (err,data) => {
+        res.json(data);
+    })
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
